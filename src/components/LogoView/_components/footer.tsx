99e5a62c3c709a0bbbb7862195307bd4a50d375c
George Loukas <georgios.a.loukas@gmail.com>
2026-07-15 18:55:09 +0300

    minor

--- src/components/LogoView/_components/footer.tsx
+++ src/components/LogoView/_components/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <div className="flex justify-center items-center h-1/5 border-t border-foreground p-8">
       <div className="flex flex-col justify-center items-center gap-4">
         <PhysicalButton redirectPath="/main-menu" />
-        <Typography type="h4">Press a Button to wake up</Typography>
+        <Typography type="h4">Press a Button</Typography>
       </div>
     </div>
   );
